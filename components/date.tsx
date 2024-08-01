38588126dec9285d1f28854e2417cb47ffe3051e
Nikita_Patel <npatel@payitgov.com>
2024-08-01 15:42:20 -0500

    onChange listener

--- components/date.tsx
+++ components/date.tsx
@@ -1,12 +1,18 @@
 import React from 'react'
+
 type Prop = {
   defaultDate: string; changeDate: (updated: Date) => void; labelText: string;
 }
-export default function DatePicker({defaultDate, labelText}:Prop) {
+
+export default function DatePicker({defaultDate, changeDate, labelText}:Prop) {
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const newDate = new Date(event.target.value);
+    changeDate(newDate);
+  }
   return (
     <div> 
       <label htmlFor="Date Picker">{labelText}</label>
-      <input type="date" id="Date Picker" name="Date Picker" defaultValue = {defaultDate}/>
+      <input type="date" id="Date Picker" name="Date Picker" defaultValue = {defaultDate} onChange={handleChange}/>
     </div>
   )
 }
